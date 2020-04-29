c79dca60f9d42a6947bfc8b8a814ce219ea4151c
pratik-1999 <pratikpandav28@gmail.com>
2020-04-29 18:48:11 +0530

    Problem related to combinations with replacement added

---
+++ possible_squads.py
@@ -0,0 +1,26 @@
+import itertools
+import numpy as np
+
+def possible_formations(N, K):
+    numbers = np.array(list(range(1,N+1)))
+    roi = numbers[numbers>=K] 
+    if K>N:
+        return 0
+
+    if K>N/2:
+        return 1
+    results = []
+    for i in range(1, (N//K)+1):
+        li = [seq for seq in itertools.combinations_with_replacement(roi, i) if sum(seq)==N]
+        # itertools.chain(results, li)
+        results += li
+    return len(results)        
+
+
+
+# Write your code here
+if __name__ =="__main__":
+    T = int(input())
+    for _ in range(T):
+        N, K = map(int, input().split())
+        print(possible_formations(N, K))
